883aa1531bb0e58c55128b3decea93b180391394
Miguel Savignano <miguel.savignano@the-cocktail.com>
2018-06-03 17:10:53 +0200

    age_value, gender_value, ocupation_value

--- lib/calculator.py
+++ lib/calculator.py
@@ -1,4 +1,4 @@
-def perform(standar_time, age_value, gender_value, profecion_value, comorbidity_value=1.0  ):
-    return standar_time * ((age_value + gender_value + profecion_value)/ 3) * comorbidity_value
+def perform(standar_time, age_value, gender_value, ocupation_value, comorbidity_value=1.0  ):
+    return standar_time * ((age_value + gender_value + ocupation_value)/ 3) * comorbidity_value
 
 # print(perform(90, 0.97, 0.99, 1.09, 1.05))
--- lib/coefficients_repository.py
+++ lib/coefficients_repository.py
@@ -1,7 +1,32 @@
 import pandas as pd
+
 def optime_time_value(code=""):
-  df = pd.read_csv("data/Aplicación tiempos óptimos.csv")
-  time = df[df["CÓDIGO "] == code].head(1).iloc[0][4]
-  return time
+    df = pd.read_csv("data/TE por DIAGNÓSTICO.csv")
+    time = df[df["CÓDIGO "] == code].head(1).iloc[0][4]
+    return time
+
+def age_value(code="", age_rage=""):
+    code = __supecode__(code)
+    df = pd.read_csv("data/FC edad.csv")
+    value = df[(df["CÓDIGO"] == code) & (df["GRUPO EDAD"] == age_rage)].head(1).iloc[0][3]
+    return value
+
+def gender_value(code="", gender=""):
+    code = __supecode__(code)
+    df = pd.read_csv("data/FC sexo.csv")
+    value = df[(df["CÓDIGO"] == code) & (df["SEXO"] == gender)].head(1).iloc[0][3]
+    return value
+
+def ocupation_value(code="", ocupation_code=""):
+    code = __supecode__(code)
+    df = pd.read_csv("data/FC ocupación.csv")
+    value = df[(df["CÓDIGO"] == code) & (df["GRUPO DE OCUPACIÓN"] == int(ocupation_code))].head(1).iloc[0]["RATIO"]
+    return value
+
+def __supecode__(code):
+    return code.split(".")[0]
 
 print(optime_time_value("I21.1"))
+print(age_value("I21.1", "26-35"))
+print(gender_value("I21.1", "hombre"))
+print(ocupation_value("I21.1", "11"))
