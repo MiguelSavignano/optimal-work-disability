049bb865d77dfdeff76b0ac10128ecd8eb0bcfeb
Miguel Savignano <miguel.savignano@the-cocktail.com>
2018-06-03 14:46:53 +0200

    calculation

--- lib/calculator.py
+++ lib/calculator.py
@@ -1,1 +1,4 @@
-def perform():
+def perform(standar_time, age_value, gender_value, profecion_value, comorbidity_value  ):
+    return standar_time * ((age_value + gender_value + profecion_value)/ 3) * comorbidity_value
+
+# print(perform(90, 0.97, 0.99, 1.09, 1.05))
