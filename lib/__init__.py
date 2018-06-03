8789db96fab578557ae18ce2722d93219e587e64
Miguel Savignano <miguel.savignano@the-cocktail.com>
2018-06-03 18:19:08 +0200

    init script

--- lib/__init__.py
+++ lib/__init__.py
@@ -14,5 +14,6 @@ def run(code, age_rage, gender, ocupation_code, second_code):
       comorbidity_value = 1.0
     return calculator.perform(standar_time, age_value, gender_value, ocupation_value, comorbidity_value)
 
-result = run(code="I21.1", age_rage="26-35", gender="hombre", ocupation_code="11", second_code="E11")
-print(result)
+if __name__ == "__main__":
+    result = run(code="I21.1", age_rage="26-35", gender="hombre", ocupation_code="11", second_code="E11")
+    print(result)
