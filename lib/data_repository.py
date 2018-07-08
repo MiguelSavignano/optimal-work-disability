f05a29ec2231d829a2f8255b960706d04eff8b75
Miguel Savignano <migue.masx@gmail.com>
2018-07-08 21:49:56 +0200

    normalize csv keys

--- lib/data_repository.py
+++ lib/data_repository.py
@@ -2,28 +2,28 @@ import pandas as pd
 from flask import json
 
 def all_diseases():
-    return pd.read_csv("data/TE por DIAGNÓSTICO.csv").to_json(orient='records')
+    return pd.read_csv("data/TE por DIAGNÓSTICO.csv").to_dict(orient='records')
 
 def all_age_rage():
-    return json.dumps([{"name": "16-25"}, {"name": "26-35"}, {"name": "36-45"}, {"name": "46-55"}, {"name": "56-65"}])
+    return [{"name": "16-25"}, {"name": "26-35"}, {"name": "36-45"}, {"name": "46-55"}, {"name": "56-65"}]
 
 def all_gender_rage():
-    return json.dumps([{"name": "hombre"}, {"name": "mujer"}])
+    return [{"name": "hombre"}, {"name": "mujer"}]
 
 def all_ocupation():
-    return pd.read_csv("data/FC capitulos y ocupacion.csv").drop_duplicates(subset=['GRUPO DE OCUPACION']) \
-    .to_json(orient="records",force_ascii=False)
+    return pd.read_csv("data/FC capitulos y ocupacion.csv").drop_duplicates(subset=['grupo_de_ocupacion']) \
+    .to_dict(orient="records")
 
 def optime_time_value(code=""):
     df = pd.read_csv("data/TE por DIAGNÓSTICO.csv")
-    time = df[df["CODIGO"] == code].head(1).iloc[0][4]
+    time = df[df["codigo"] == code].head(1).iloc[0][4]
     return time
 
 def age_value(code="", age_rage=""):
     code = __supecode__(code)
     df = pd.read_csv("data/FC edad.csv")
     try:
-        value = df[(df["CODIGO"] == code) & (df["GRUPO EDAD"] == age_rage)].iloc[0][3]
+        value = df[(df["codigo"] == code) & (df["grupo_edad"] == age_rage)].iloc[0][3]
     except:
         raise AgeValueException
     return value
@@ -31,14 +31,14 @@ def age_value(code="", age_rage=""):
 def gender_value(code="", gender=""):
     code = __supecode__(code)
     df = pd.read_csv("data/FC sexo.csv")
-    value = df[(df["CODIGO"] == code) & (df["SEXO"] == gender)].iloc[0][3]
+    value = df[(df["codigo"] == code) & (df["sexo"] == gender)].iloc[0][3]
     return value
 
 def ocupation_value(code="", ocupation_code=""):
     code = __supecode__(code)
     df = pd.read_csv("data/FC ocupación.csv")
     try:
-        value = df[(df["CODIGO"] == code) & (df["GRUPO DE OCUPACIÓN"] == int(ocupation_code))].iloc[0]["RATIO"]
+        value = df[(df["codigo"] == code) & (df["grupo_de_opcupacion"] == int(ocupation_code))].iloc[0]["ratio"]
     except:
         raise OcupationValueException
     return value
@@ -48,7 +48,7 @@ def comorbidity_value(first_code="", second_code=""):
     second_code = __supecode__(second_code)
     df = pd.read_csv("data/COMORBILIDAD.csv")
     try:
-        value = df[(df["CODIGO PRINCIPAL"] == first_code) & (df["CODIGO SECUNDARIO"] == second_code)].iloc[0]["RATIO"]
+        value = df[(df["codigo_principal"] == first_code) & (df["codigo_secundario"] == second_code)].iloc[0]["ratio"]
     except:
         raise ComorbidityValueException
     return value
