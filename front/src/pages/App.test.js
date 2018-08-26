b41210f7e10097c11ada4b1dfe54bb9bee54f82f
Miguel Savignano <migue.masx@gmail.com>
2018-08-27 00:57:51 +0200

    use fetch-vcr in test and async test for Home page

--- front/src/pages/App.test.js
+++ front/src/pages/App.test.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Confirmationform from './Confirmationform';
-// import Home from './Home';
+import Home from './Home';
+import fetch from 'fetch-vcr';
+
+fetch.configure({
+  fixturePath: './_fixtures',
+  mode: 'cache'
+})
+
+global.fetch = fetch
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -9,8 +17,11 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<Home />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
+it('Home renders without crashing', (done) => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Home />, div);
+  setTimeout(() => {
+    ReactDOM.unmountComponentAtNode(div);
+    done()
+  }, 500)
+});
